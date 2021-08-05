0377dd5944e902370c24550fe64026bce1cbe91d
VinecntLin <78530513+VinecntLin@users.noreply.github.com>
2021-08-05 15:18:44 +1000

    finish the question

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello World");
+        System.out.println("Red vs. Blue");
+<<<<<<< HEAD
+
+=======
+>>>>>>> 2e7595c467f0f05162ef50d7fcd327c57534f19f
     }
 }
